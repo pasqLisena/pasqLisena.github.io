1188c88e2b2bef688d08eca200ad32e34480d514
Pasquale <pasq.lisena@gmail.com>
2015-04-20 12:11:32 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(window).scroll(function (e) {
+    var scroll = $(window).scrollTop();
+    var $header = $('#header');
+    var alpha = scroll / ($('#home').outerHeight() - 2 * $header.outerHeight())
+    $header.toggleClass('opaque', alpha >= 1);
+    $header.css('background', 'rgba(54,54,54,' + alpha + ')');
+}).scroll();
