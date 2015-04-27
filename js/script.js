f015ad889a2bd19159103a2cd88781da16607cf0
Pasquale <pasq.lisena@gmail.com>
2015-04-27 16:30:53 +0200

    complete v1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-$(window).scroll(function (e) {
-    var scroll = $(window).scrollTop();
-    var $header = $('#header');
-    var alpha = scroll / ($('#home').outerHeight() - 2 * $header.outerHeight())
-    $header.toggleClass('opaque', alpha >= 1);
-    $header.css('background', 'rgba(54,54,54,' + alpha + ')');
-}).scroll();
+$(document).ready(function(){
+    $(window).on('scroll', function (e) {
+        var scroll = $(window).scrollTop();
+        var $header = $('#header');
+        var alpha = scroll / ($('#home').outerHeight() - 2 * $header.outerHeight())
+        $header.toggleClass('opaque', alpha >= 1);
+        $header.css('background', 'rgba(54,54,54,' + alpha + ')');
+    }).scroll();
+});
